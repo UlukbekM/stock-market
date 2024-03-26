a86eb47f34864684473e28d6ee530b0d1824f8fb
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2024-03-25 22:07:49 -0400

    signup fix

--- app/auth/sign-up/route.tsx
+++ app/auth/sign-up/route.tsx
@@ -23,6 +23,7 @@ export async function POST(request: Request) {
     })
 
     if(error) {
+        // console.log(error)
         return NextResponse.redirect(requestUrl.origin, {
             status: 301,
         })
